d98b795be55e86a2d213d7ed6e8fe37c859cf537
noise2 <b.g.dariush@gmail.com>
2016-07-05 22:02:17 +0430

    @ index.php: multi lang is in effect

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 ﻿<?php
     $uri_param = '_uri';
-    $params = ['lang' => 'en', 'supported_langs' => ['en' => 'English', 'fa' => 'Farsi']];
+    $params = [
+        'lang' => 'en',
+        'supported_langs' => [
+            'en' => ['English', 'ltr'],
+            'fa' => ['Farsi', 'rtl']
+        ],
+        'dir' => 'auto'
+    ];
     if(isset($_REQUEST[$uri_param])) {
         $vars = array_values(array_filter(explode('/', $_REQUEST[$uri_param])));
-        if(count($vars) && array_key_exists($vars[0], $params['supported_langs']))
-            $params['lang'] = strtolower(array_shift($vars));
+        if(count($vars) && array_key_exists(strtolower($vars[0]), $params['supported_langs'])) {
+            $lang = strtolower(array_shift($vars));
+            $params['dir'] = $params['supported_langs'][$lang][1];
+            $params['lang'] =  $lang;
+        }
+
     }
+    $_trans = parse_ini_file('conf/trans.ini', true);
+    function get_trans($label) { global $_trans, $params; return @$_trans[$params['lang']][$label]; }
 ?>
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?= $params['lang'] ?>" xml:lang="<?= $params['lang'] ?>" dir='<?= $params['dir'] ?>'>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 
-<title>Virahub - Coming Soon</title>
+<link rel="alternate" href="//virahub.net" hreflang="en-US" />
+<link rel="alternate" href="//virahub.net/en" hreflang="en-US" />
+<link rel="alternate" href="//virahub.net/fa" hreflang="fa-IR" />
+
+
+<title><?= get_trans('title') ?></title>
 <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
 
 <!--CSS-->
@@ -27,44 +46,43 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
 <script src="/js/jquery.plugin.js" type="text/javascript"></script>
 <script src="/js/jquery.countdown.js" type="text/javascript"></script>
+<script src="/js/jquery.countdown-<?= $params['lang'] ?>.js" type="text/javascript"></script>
 <script type="text/javascript">
 $(function () { $('#defaultCountdown').countdown({until: new Date(2017, 2, 3)}); });
 </script>
 <style type="text/css">
     .breadcrumb.lang {background-color: transparent;}
     .breadcrumb.lang li a:hover{text-decoration: none}
-    .breadcrumb.lang li.active a {border-bottom: 1px solid #e7e7e7; padding-bottom: 5px;}
+    .breadcrumb.lang li.active a {border-bottom: 1px solid #e7e7e7; padding-bottom: 5px; color: #444;}
 </style>
 </head>
 
 <body>
-    <!--WRAP-->
-    <div id="wrap">
-        <!--CONTAINER-->
-        <div class="container">
-            <div style='text-align: right'>
-                <ul class="breadcrumb lang" style=''>
-                    <?php foreach ($params['supported_langs'] as $lang => $value) {
-                        echo '<li'.($params['lang'] == $lang ? ' class="active"' : '').'>';
-                        echo "<a href='/$lang'>$value</a>";
-                        echo "</li>";
-                    }?>
-                </ul>
-                </div>
-            <img src="/images/logo.png" alt="Vira Logo" class="image-align" height="160" />
-            <h1 style='line-height: 40px'>
-                <brand style='font-variant: small-caps;'>Vira</brand><br />
-                <span style='margin-bottom:-100px;font-family: "Dancing Script"'>
-                    The smart Industry
-                </span>
-            </h1>
-            <p>
-                Our site is currently <strong>under construction</strong> but we are working hard<br/> to create a new and fresh design.
-            </p>
-            <div id="defaultCountdown"></div>
-        </div>
-        <!--/CONTAINER-->
+    <!--CONTAINER-->
+    <div class="container-fluid">
+        <div style='text-align: right'>
+            <ul class="breadcrumb lang" style=''>
+                <?php foreach ($params['supported_langs'] as $lang => $value) {
+                    echo '<li'.($params['lang'] == $lang ? ' class="active"' : '').'>';
+                    echo "<a href='/$lang'>${value[0]}</a>";
+                    echo "</li>";
+                }?>
+            </ul>
+            </div>
+        <div class="clear-fix" style='margin-top:50px;'></div>
+        <img src="/images/logo.png" alt="Vira Logo" class="image-align" height="160"/>
+        <h1 style='line-height: 40px'>
+            <brand style='font-variant: small-caps;'><?= get_trans('company_name') ?></brand><br />
+            <span style='margin-bottom:-100px;font-family: "Dancing Script"'>
+                <?= get_trans('company_slogans') ?>
+            </span>
+        </h1>
+        <p style='padding:20px 0 20px 0'>
+            <?= get_trans('under_construction_text') ?><br />
+            <strong><?= get_trans('contact_us') ?></strong>
+        </p>
+        <div id="defaultCountdown"></div>
     </div>
-    <!--/WRAP-->
+    <!--/CONTAINER-->
 </body>
 </html>
