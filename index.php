fff58952d6ab791d15355d6d39e67322fa7ea467
noise2 <b.g.dariush@gmail.com>
2016-07-05 18:29:47 +0430

    + /index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<title>Virahub - Coming Soon</title>
+<link rel="shortcut icon" href="/favicon.ico" type="image/ico">
+
+<!--CSS-->
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/bootstrap-light.css">
+<link href='//fonts.googleapis.com/css?family=Maitree' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+<!--/CSS-->
+
+<!--JS-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/js/jquery.plugin.js"></script>
+<script src="/js/jquery.countdown.js"></script>
+<script>
+$(function () {
+        $('#defaultCountdown').countdown({until: new Date(2017, 2, 3)});
+        //Replace above date with your own, to find out more visit http://keith-wood.name/countdown.html
+});
+</script>
+<!--/JS-->
+
+</head>
+
+<body>
+
+<!--DARK OVERLAY-->
+<div class="overlay"></div>
+<!--/DARK OVERLAY-->
+
+<!--WRAP-->
+<div id="wrap">
+        <!--CONTAINER-->
+        <div class="container">
+                <img src="/images/logo.png" alt="Vira Logo" class="image-align" height="160" />
+                <h1 style='line-height: 40px'><BRAND style='font-variant: small-caps;'>Vira</BRAND><br /><span style='margin-bottom:-100px;font-family: "Dancing Script"'>The smart Industry</span></h1>
+                <p>Our site is currently <strong>under construction</strong> but we are working hard<br/> to create a new and fresh design.</p>
+                <div id="defaultCountdown"></div>
+        </div>
+        <!--/CONTAINER-->
+</div>
+<!--/WRAP-->
+</body>
+</html>
